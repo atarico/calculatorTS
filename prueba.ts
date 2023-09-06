aa8494e606ae20c4c506c0d46da21b98d7859d6f
atarico9 <atarico9@gmail.com>
2023-09-06 02:21:30 -0300

    initial commit using TypeScript

--- prueba.ts
+++ prueba.ts
@@ -1,24 +1,21 @@
-const suma = (a: number, b: number, printText: string) => {
+/* const suma = (a: number, b: number, printText: string) => {
     console.log(printText, a + b);
 }
 
-const parseArguments = (args: Array<string>): number[] => {
+const parseArguments = (args: Array<string>) => {
     // las formas de declarar los tipos de array son Array<(tipo de dato)> o (tipo de datos)[]
     if (args.length !== 4) throw new Error("Argumento invalido")
 
     const firstNumber = Number(args[2])
     const secondNumber = Number(args[3])
 
-    if (!isNaN(firstNumber) || !isNaN(secondNumber)) {
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
         return [
             firstNumber,
             secondNumber
         ]
     }
     throw new Error("Los valores ingresados no son correctos")
-
-
-
 }
 
 const cleanArguments = parseArguments(process.argv)
@@ -31,4 +28,7 @@ console.log(process.argv);
 const a: number = Number(cleanArguments[0])
 const b: number = Number(cleanArguments[1])
 
-suma(a, b, `Suma ${a} y ${b} y el resultado es: `);
+suma(a, b, `Suma ${a} y ${b} y el resultado es:`); */
+
+
+console.log(process.argv);
